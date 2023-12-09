0439a459c5352d3ef61fb5f2da295d8a48c4a807
LahiruHarshana <lharshana2002@gmail.com>
2023-12-09 23:05:49 +0530

    updated

--- src/main/java/lk/ijse/gdse66/AppInitalizer.java
+++ src/main/java/lk/ijse/gdse66/AppInitalizer.java
@@ -4,9 +4,9 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class AppInitalizer extends Application {
-
     @Override
     public void start(Stage stage) throws Exception {
+
 
     }
 }
---
+++ src/main/java/lk/ijse/gdse66/controller/LoginFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse66.controller;
+
+public class LoginFormController {
+}
