432071287f032c4d8bec0f124c315adf818bf92f
LahiruHarshana <lharshana2002@gmail.com>
2023-12-09 23:11:40 +0530

    updated

--- src/main/java/lk/ijse/gdse66/configaration/SessionFactoryConfig.java
+++ src/main/java/lk/ijse/gdse66/configaration/SessionFactoryConfig.java
@@ -3,11 +3,14 @@ package lk.ijse.gdse66.configaration;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Properties;
+
 public class SessionFactoryConfig {
 
     private static SessionFactoryConfig factoryConfig;
     private final SessionFactory sessionFactory;
     private SessionFactoryConfig(){
         Configuration configuration = new Configuration().configure();
+        Properties properties = new Properties();
     }
 }
