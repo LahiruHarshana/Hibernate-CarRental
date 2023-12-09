9a96880c47fb8044848ae6c6efcbdabe58708453
LahiruHarshana <lharshana2002@gmail.com>
2023-12-09 23:00:46 +0530

    updated

--- src/main/java/lk/ijse/gdse66/AppInitalizer.java
+++ src/main/java/lk/ijse/gdse66/AppInitalizer.java
@@ -1,6 +1,6 @@
-package lk.ijse;
+package lk.ijse.gdse66;
 
-public class AppInitalizer {
+public class AppInitalizer extends Application{
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
