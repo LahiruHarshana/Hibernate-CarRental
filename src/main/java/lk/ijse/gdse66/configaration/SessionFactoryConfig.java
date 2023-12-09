e39fd5f96bbe9907db90bf3f5a406d4f3051380e
LahiruHarshana <lharshana2002@gmail.com>
2023-12-09 23:15:21 +0530

    updated

--- src/main/java/lk/ijse/gdse66/configaration/SessionFactoryConfig.java
+++ src/main/java/lk/ijse/gdse66/configaration/SessionFactoryConfig.java
@@ -3,14 +3,18 @@ package lk.ijse.gdse66.configaration;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.IOException;
 import java.util.Properties;
 
 public class SessionFactoryConfig {
 
     private static SessionFactoryConfig factoryConfig;
     private final SessionFactory sessionFactory;
-    private SessionFactoryConfig(){
+    private SessionFactoryConfig() throws IOException {
         Configuration configuration = new Configuration().configure();
         Properties properties = new Properties();
+        properties.load(ClassLoader.getSystemClassLoader().getResourceAsStream("hibernate.properties"));
+
+        configuration.addAnnotatedClass();
     }
 }
