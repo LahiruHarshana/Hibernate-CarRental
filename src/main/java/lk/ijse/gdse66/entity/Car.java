db307137efe3684d9bf322f6372ba1a262724baf
LahiruHarshana <lharshana2002@gmail.com>
2023-12-09 23:16:44 +0530

    updated

--- src/main/java/lk/ijse/gdse66/entity/Car.java
+++ src/main/java/lk/ijse/gdse66/entity/Car.java
@@ -1,11 +1,18 @@
 package lk.ijse.gdse66.entity;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "car")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Car {
 }
