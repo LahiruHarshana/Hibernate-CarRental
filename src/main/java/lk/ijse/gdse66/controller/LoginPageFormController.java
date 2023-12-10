6bfd73b47159a2d2c4c0433afe4298dce3dde575
LahiruHarshana <lharshana2002@gmail.com>
2023-12-10 23:10:36 +0530

    updated

--- src/main/java/lk/ijse/gdse66/controller/LoginPageFormController.java
+++ src/main/java/lk/ijse/gdse66/controller/LoginPageFormController.java
@@ -1,4 +1,60 @@
 package lk.ijse.gdse66.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginPageFormController {
+    @FXML
+    private JFXButton loginBtn;
+
+    @FXML
+    private PasswordField pwTxt;
+
+    @FXML
+    private Button pwViewBtn;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private JFXButton signUpBtn;
+
+    @FXML
+    private TextField userNameTxt;
+
+    @FXML
+    private ImageView viewImg;
+
+    @FXML
+    void loginBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pwTxtOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pwViewBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpBtnOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userNameTxtOnAction(ActionEvent event) {
+
+    }
+
+
 }
