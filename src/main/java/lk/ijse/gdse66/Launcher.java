f183f2c7282337d1b4494ce9cf13d7a3b6bb5a40
LahiruHarshana <lharshana2002@gmail.com>
2024-01-14 22:47:06 +0530

    updated

--- src/main/java/lk/ijse/gdse66/Launcher.java
+++ src/main/java/lk/ijse/gdse66/Launcher.java
@@ -11,7 +11,6 @@ public class Launcher extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage stage) throws Exception {
 
